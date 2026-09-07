1aa42fef0fdbdc20facd9f041e0466b25597cb10
Souptik <111554072+Souptik-Mu@users.noreply.github.com>
2026-09-07 19:32:49 +0530

    Project created

---
+++ LeafDocBackend/src/main/java/com/example/leafdoc/LeafDocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.leafdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafDocBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafDocBackendApplication.class, args);
+    }
+
+}
---
+++ LeafDocBackend/src/test/java/com/example/leafdoc/LeafDocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leafdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafDocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
